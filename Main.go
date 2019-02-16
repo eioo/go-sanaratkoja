df9ae58df16e7f153d727b71643fe470849f09b4
Kallu609 <khirvola@gmail.com>
2019-02-16 19:01:59 +0200

    fix indent

--- Main.go
+++ Main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-	"time"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "time"
 )
 
 type Position struct {
-	x, y int
+    x, y int
 }
 
 type Board [][]string
@@ -18,181 +18,180 @@ type Board [][]string
 var board Board
 
 func loadWords(filename string) []string {
-	var words []string
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+    var words []string
+    file, err := os.Open(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		word := strings.TrimSpace(scanner.Text())
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        word := strings.TrimSpace(scanner.Text())
 
-		if len(word) > 0 {
-			words = append(words, word)
-		}
-	}
+        if len(word) > 0 {
+            words = append(words, word)
+        }
+    }
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 
-	return words
+    return words
 }
 
 func isSubset(of, arr *[]string) bool {
-	set := make(map[string]int)
+    set := make(map[string]int)
 
-	for _, value := range *of {
-		set[value] += 1
-	}
+    for _, value := range *of {
+        set[value] += 1
+    }
 
-	for _, value := range *arr {
-		if count, found := set[value]; !found {
-			return false
-		} else if count < 1 {
-			return false
-		} else {
-			set[value] = count - 1
-		}
-	}
+    for _, value := range *arr {
+        if count, found := set[value]; !found {
+            return false
+        } else if count < 1 {
+            return false
+        } else {
+            set[value] = count - 1
+        }
+    }
 
-	return true
+    return true
 }
 
 func (board *Board) get(x, y int) string {
-	return (*board)[y][x]
+    return (*board)[y][x]
 }
 
 func (board *Board) toArray() []string {
-	var allChars []string
+    var allChars []string
 
-	for _, row := range *board {
-		for _, col := range row {
-			allChars = append(allChars, col)
-		}
-	}
+    for _, row := range *board {
+        for _, col := range row {
+            allChars = append(allChars, col)
+        }
+    }
 
-	return allChars
+    return allChars
 }
 
 func (board *Board) findCharPositions(char string) []Position {
-	var positions []Position
+    var positions []Position
 
-	for y, row := range *board {
-		for x := range row {
-			if board.get(x, y) == char {
-				pos := Position { x, y }
-				positions = append(positions, pos)
-			}
-		}
-	}
+    for y, row := range *board {
+        for x := range row {
+            if board.get(x, y) == char {
+                pos := Position { x, y }
+                positions = append(positions, pos)
+            }
+        }
+    }
 
-	return positions
+    return positions
 }
 
 func (pos *Position) getNeighbours() []Position {
-	var n []Position
-	boardSize := len(board)
+    var n []Position
+    boardSize := len(board)
 
-	if pos.y-1 >= 0 && pos.x-1 >= 0					{ n = append(n, Position {pos.x-1, pos.y-1}) }
-	if pos.y-1 >= 0 && pos.x+1 < boardSize 			{ n = append(n, Position {pos.x+1, pos.y-1}) }
-	if pos.y-1 >= 0 					  			{ n = append(n, Position {pos.x, pos.y-1}) }
+    if pos.y-1 >= 0 && pos.x-1 >= 0               { n = append(n, Position {pos.x-1, pos.y-1}) }
+    if pos.y-1 >= 0 && pos.x+1 < boardSize        { n = append(n, Position {pos.x+1, pos.y-1}) }
+    if pos.y-1 >= 0                               { n = append(n, Position {pos.x, pos.y-1}) }
 
-	if pos.x-1 >= 0 								{ n = append(n, Position {pos.x-1, pos.y}) }
-	if pos.x+1 < boardSize							{ n = append(n, Position {pos.x+1, pos.y}) }
+    if pos.x-1 >= 0                               { n = append(n, Position {pos.x-1, pos.y}) }
+    if pos.x+1 < boardSize                        { n = append(n, Position {pos.x+1, pos.y}) }
 
-	if pos.y+1 < boardSize && pos.x-1 >= 0        	{ n = append(n, Position {pos.x-1, pos.y+1}) }
-	if pos.y+1 < boardSize && pos.x+1 < boardSize 	{ n = append(n, Position {pos.x+1, pos.y+1}) }
-	if pos.y+1 < boardSize 							{ n = append(n, Position {pos.x, pos.y+1}) }
+    if pos.y+1 < boardSize && pos.x-1 >= 0        { n = append(n, Position {pos.x-1, pos.y+1}) }
+    if pos.y+1 < boardSize                        { n = append(n, Position {pos.x, pos.y+1}) }
+    if pos.y+1 < boardSize && pos.x+1 < boardSize { n = append(n, Position {pos.x+1, pos.y+1}) }
 
-	return n
+    return n
 }
 
 func findWords(board *Board, words *[]string) []string {
-	var foundWords []string
-	allChars := board.toArray()
+    var foundWords []string
+    allChars := board.toArray()
 
-	for _, word := range *words {
-		var chars []string
-		wordLength := len([]rune(word))
+    for _, word := range *words {
+        var chars []string
+        wordLength := len([]rune(word))
 
-		for _, c := range word {
-			chars = append(chars, string(c))
-		}
+        for _, c := range word {
+            chars = append(chars, string(c))
+        }
 
-		if !isSubset(&allChars, &chars) {
-			continue
-		}
+        if !isSubset(&allChars, &chars) {
+            continue
+        }
 
-		// Do board search
-		startPositions := board.findCharPositions(chars[0])
+        // Do board search
+        startPositions := board.findCharPositions(chars[0])
 
-		for _, startPos := range startPositions {
-			traversedPositions := []Position { startPos }
-			charIndex := 1
+        for _, startPos := range startPositions {
+            traversedPositions := []Position { startPos }
+            charIndex := 1
 
-			for charIndex < wordLength {
-				currentPosition := traversedPositions[len(traversedPositions) - 1]
-				neighbours := currentPosition.getNeighbours()
-				currentChar := string([]rune(word)[charIndex])
-				charFound := false
+            for charIndex < wordLength {
+                currentPosition := traversedPositions[len(traversedPositions) - 1]
+                neighbours := currentPosition.getNeighbours()
+                currentChar := string([]rune(word)[charIndex])
+                charFound := false
 
-				for _, neighbour := range neighbours {
-					skip := false
+                for _, neighbour := range neighbours {
+                    skip := false
 
-					for _, traversedPos := range traversedPositions {
-						if neighbour == traversedPos {
-							skip = true
-							break
-						}
-					}
+                    for _, traversedPos := range traversedPositions {
+                        if neighbour == traversedPos {
+                            skip = true
+                            break
+                        }
+                    }
 
-					if skip {
-						continue
-					}
+                    if skip {
+                        continue
+                    }
 
-					neighbourChar := board.get(neighbour.x, neighbour.y)
+                    neighbourChar := board.get(neighbour.x, neighbour.y)
 
-					if neighbourChar == currentChar {
-						traversedPositions = append(traversedPositions, Position { neighbour.x, neighbour.y })
-						charIndex++
-						charFound = true
-						break
-					}
-				}
+                    if neighbourChar == currentChar {
+                        traversedPositions = append(traversedPositions, Position { neighbour.x, neighbour.y })
+                        charIndex++
+                        charFound = true
+                        break
+                    }
+                }
 
-				if !charFound {
-					break
-				}
-			}
+                if !charFound {
+                    break
+                }
+            }
 
-			// Word possible
-			if charIndex == wordLength {
-				foundWords = append(foundWords, word)
-				break
-			}
-		}
-	}
+            if charIndex == wordLength {
+                foundWords = append(foundWords, word)
+                break
+            }
+        }
+    }
 
-	return foundWords
+    return foundWords
 }
 
 func main() {
-	words := loadWords("kotus_sanat.txt")
+    words := loadWords("kotus_sanat.txt")
 
-	board = Board {
-		{ "S", "U", "L", "N" },
-		{ "I", "I", "I", "K" },
-		{ "Ä", "D", "N", "Ä" },
-		{ "T", "K", "Ä", "K" },
-	}
+    board = Board {
+        { "S", "U", "L", "N" },
+        { "I", "I", "I", "K" },
+        { "Ä", "D", "N", "Ä" },
+        { "T", "K", "Ä", "K" },
+    }
 
-	start := time.Now()
-	foundWords := findWords(&board, &words)
-	delta := time.Since(start)
+    start := time.Now()
+    foundWords := findWords(&board, &words)
+    delta := time.Since(start)
 
-	fmt.Println(strings.Join(foundWords, "\n"))
-	fmt.Printf("\nFound words: %v\nTook: %v", len(foundWords), delta)
+    fmt.Println(strings.Join(foundWords, "\n"))
+    fmt.Printf("\nFound words: %v\nTook: %v", len(foundWords), delta)
 }
